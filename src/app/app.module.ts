9c58b78149fc7bd3517ac437a3aebd7bdeb25449
andre.tirado <andre.tirado@xelapan.com>
2018-04-12 10:48:58 -0600

    Tutoria de Ionic, se sube a git para hacer el link de la app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
+import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic'; //Se importan las paginas
 import { ItemDetailsPage } from '../pages/item-details/item-details';
 import { ListPage } from '../pages/list/list';
 
@@ -11,7 +11,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
-  declarations: [
+  declarations: [ //Declaracion de las paginas
     MyApp,
     HelloIonicPage,
     ItemDetailsPage,
